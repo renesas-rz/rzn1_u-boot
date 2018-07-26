887698f2e85b7dab07ef649976774520b5a1fd0e
Phil Edworthy <phil.edworthy@renesas.com>
2018-07-26 15:56:48 +0100

    arm: asm: dma-mapping: Do cache flush/invalidate for dma_map_single
    This allows drivers that call the Linux dma_map_single() function to
    work in U-Boot with the data cache turned on.
    
    Signed-off-by: Phil Edworthy <phil.edworthy@renesas.com>

--- arch/arm/include/asm/dma-mapping.h
+++ arch/arm/include/asm/dma-mapping.h
@@ -16,26 +16,42 @@ enum dma_data_direction {
 	DMA_FROM_DEVICE		= 2,
 };
 
-static inline void *dma_alloc_coherent(size_t len, unsigned long *handle)
-{
-	*handle = (unsigned long)memalign(ARCH_DMA_MINALIGN, len);
-	return (void *)*handle;
-}
-
-static inline void dma_free_coherent(void *addr)
-{
-	free(addr);
-}
-
+/* U-Boot has a 1:1 mapping between physical and virtual addresses */
 static inline unsigned long dma_map_single(volatile void *vaddr, size_t len,
 					   enum dma_data_direction dir)
 {
+	unsigned long start = (unsigned long)vaddr & ~(ARCH_DMA_MINALIGN - 1);
+	unsigned long end = (unsigned long)vaddr + len - 1;
+
+	end = (end + ARCH_DMA_MINALIGN) & ~(ARCH_DMA_MINALIGN - 1);
+
+	if (len) {
+		if (dir != DMA_FROM_DEVICE)
+			flush_dcache_range(start, end);
+		if (dir != DMA_TO_DEVICE)
+			invalidate_dcache_range(start, end);
+	}
+
 	return (unsigned long)vaddr;
 }
 
 static inline void dma_unmap_single(volatile void *vaddr, size_t len,
 				    unsigned long paddr)
 {
+}
+
+static inline void *dma_alloc_coherent(size_t len, unsigned long *handle)
+{
+	void *vaddr = memalign(ARCH_DMA_MINALIGN, len);
+
+	*handle = dma_map_single(vaddr, len, DMA_TO_DEVICE);
+
+	return vaddr;
+}
+
+static inline void dma_free_coherent(void *addr)
+{
+	free(addr);
 }
 
 #endif /* __ASM_ARM_DMA_MAPPING_H */
